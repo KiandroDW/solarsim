0437ff2008c5356dfe9cc5ba5620f06b7b15f7ac
kiandro <kiandro.de.waegenaere@gmail.com>
2023-09-09 17:50:48 +0200

    V1 working

--- main.py
+++ main.py
@@ -6,21 +6,27 @@ import sys
 from pygame.math import Vector2
 import ctypes
 
-# Create a popup screen
-ctypes.windll.shcore.SetProcessDpiAwareness(1)
-pygame.init()
-fpsClock = pygame.time.Clock()
-windowdim = Vector2(1500, 1000)
-screen = pygame.display.set_mode((int(windowdim.x), int(windowdim.y)), pygame.RESIZABLE)
-
-
-# RefDay is the day + hour of which the simulation is based
+# Choose if you want to see the "INNER" or "OUTER" planets
+mode = "INNER"
+# The filename of the JSON file with planetdata
+filename = "planetdata.json"
+# RefDay: (year, month, date, hour) is the time on which the simulation is based. If you change it,
+# change the Start Angle in the JSON file to the correct angle on that date if you want it to be accurate
 RefDay = datetime.datetime(2023, 3, 20, 22)
+# These 3 are the only things you should change to not brake it
+
 Today = datetime.datetime(datetime.datetime.today().year,
                           datetime.datetime.today().month,
                           datetime.datetime.today().day,
                           datetime.datetime.today().hour)
 
+
+# Create a popup screen
+ctypes.windll.shcore.SetProcessDpiAwareness(1)
+pygame.init()
+fpsClock = pygame.time.Clock()
+windowdim = Vector2(1000, 1000)
+screen = pygame.display.set_mode((int(windowdim.x), int(windowdim.y)), pygame.RESIZABLE)
 
 # Main class that calculates everything
 class Planet:
@@ -43,8 +49,9 @@ class Planet:
 
     This calculates all elements of the orbit.
     """
+
     def __init__(self, Name: str, StartAngle: float, PerihelionDistance: float,
-                  PerihelionAngle: float, period: float, eccentricity: float, radius: float, color: str):
+                 PerihelionAngle: float, period: float, eccentricity: float, radius: float, color: str):
         # Save all the settings of the planet
         self.startA = StartAngle
         self.periD = PerihelionDistance
@@ -57,6 +64,18 @@ class Planet:
         colorl = [int(i) for i in colorl]
         self.color = colorl
         planets.append(self)
+
+        # Define the different scales for the mode
+        if mode == "INNER":
+            self.scale = 600000
+            self.rscale = 200
+            self.solarradius = 40
+        elif mode == "OUTER":
+            self.scale = 10000000
+            self.rscale = 3000
+            self.solarradius = 30
+        else:
+            assert 1 == 0, "Wrong mode, choose 'INNER' or 'OUTER'"
 
         # semi-major axis, semi-minor axis
         self.Major = self.periD / (1 - self.ecc)
@@ -82,7 +101,7 @@ class Planet:
         self.coords = []
         for i in x:
             pos = x.index(i)
-            self.coords.append((x[pos] / 500000 + 750, y[pos] / 500000 + 500))
+            self.coords.append((x[pos] / self.scale + int(windowdim.x) / 2, y[pos] / self.scale + int(windowdim.y) / 2))
 
         # Calculate the x-coordinate of the planet when it's at y=0
         closesty = min([math.fabs(i) for i in y])
@@ -110,18 +129,20 @@ class Planet:
 
         # Calculate the position of the planet at the start
         self.x = self.Major * math.cos(- self.periA - self.startA + self.Anglediff) * math.cos(self.periA) - \
-                 self.Minor * math.sin(- self.periA - self.startA + self.Anglediff) * math.sin(self.periA) - self.cx
+            self.Minor * math.sin(- self.periA - self.startA + self.Anglediff) * math.sin(self.periA) - self.cx
         self.y = self.Major * math.cos(- self.periA - self.startA + self.Anglediff) * math.sin(self.periA) + \
-                 self.Minor * math.sin(- self.periA - self.startA + self.Anglediff) * math.cos(self.periA) - self.cy
+            self.Minor * math.sin(- self.periA - self.startA + self.Anglediff) * math.cos(self.periA) - self.cy
 
         # Calculate the error-factor on the position of the planet, this error happens because this program calculates
         # the angular velocity manually
         totalw = 0
         for i in range(int(self.period * 24)):
             x = self.Major * math.cos(- self.periA - self.startA + self.Anglediff - totalw) * math.cos(self.periA) - \
-                self.Minor * math.sin(- self.periA - self.startA + self.Anglediff - totalw) * math.sin(self.periA) - self.cx
+                self.Minor * math.sin(- self.periA - self.startA + self.Anglediff - totalw) * math.sin(
+                self.periA) - self.cx
             y = self.Major * math.cos(- self.periA - self.startA + self.Anglediff - totalw) * math.sin(self.periA) + \
-                self.Minor * math.sin(- self.periA - self.startA + self.Anglediff - totalw) * math.cos(self.periA) - self.cy
+                self.Minor * math.sin(- self.periA - self.startA + self.Anglediff - totalw) * math.cos(
+                self.periA) - self.cy
             GM = 6.67e-11 * 1.989e30
             velocity = math.sqrt(GM * (2 / (math.sqrt(x ** 2 + y ** 2) * 1000) - 1 / (self.Major * 1000)))
             avelocity = velocity / math.sqrt((x * 1000) ** 2 + (y * 1000) ** 2) * 3600
@@ -132,24 +153,51 @@ class Planet:
         self.totalw = 0
 
     def process(self):
-        pygame.draw.polygon(screen, self.color, self.coords, width=5)
-        pygame.draw.circle(screen, self.color, (self.x / 500000 + 750, self.y / 500000 + 500), radius=self.radius / 200)
+        # Add the outer planets to the inner planet mode
+        if mode == 'INNER' and self.name in ["Jupiter", "Saturn", "Uranus", "Neptune"]:
+            if (0 <= ((self.startA + self.totalw) % (2 * math.pi)) <= math.pi / 4) or \
+                    (7 * math.pi / 4 <= ((self.startA + self.totalw) % (2 * math.pi)) <= 2 * math.pi):
+                pygame.draw.circle(screen, self.color,
+                                   (975, - 475 * math.tan(self.startA + self.totalw) + 500),
+                                   radius=self.radius / 3000)
+            elif math.pi / 4 <= ((self.startA + self.totalw) % (2 * math.pi)) <= 3 * math.pi / 4:
+                pygame.draw.circle(screen, self.color,
+                                   (475 / math.tan(self.startA + self.totalw) + 500, 25),
+                                   radius=self.radius / 3000)
+            elif 3 * math.pi / 4 <= ((self.startA + self.totalw) % (2 * math.pi)) <= 5 * math.pi / 4:
+                pygame.draw.circle(screen, self.color,
+                                   (25, 475 * math.tan(self.startA + self.totalw) + 500),
+                                   radius=self.radius / 3000)
+            else:
+                pygame.draw.circle(screen, self.color,
+                                   (- 475 / math.tan(self.startA + self.totalw) + 500, 975),
+                                   radius=self.radius / 3000)
+        else:
+            pygame.draw.polygon(screen, self.color, self.coords, width=5)
+            pygame.draw.circle(screen, self.color,
+                               (self.x / self.scale + int(windowdim.x) / 2, self.y / self.scale + int(windowdim.y) / 2),
+                               radius=self.radius / self.rscale)
         GM = 6.67e-11 * 1.989e30
         velocity = math.sqrt(GM * (2 / (math.sqrt(self.x ** 2 + self.y ** 2) * 1000) - 1 / (self.Major * 1000)))
         avelocity = velocity / math.sqrt((self.x * 1000) ** 2 + (self.y * 1000) ** 2) * spf * self.errorf
         self.totalw += avelocity
-        self.x = self.Major * math.cos(- self.periA - self.startA + self.Anglediff - self.totalw) * math.cos(self.periA) - \
-                 self.Minor * math.sin(- self.periA - self.startA + self.Anglediff - self.totalw) * math.sin(self.periA) - self.cx
-        self.y = self.Major * math.cos(- self.periA - self.startA + self.Anglediff - self.totalw) * math.sin(self.periA) + \
-                 self.Minor * math.sin(- self.periA - self.startA + self.Anglediff - self.totalw) * math.cos(self.periA) - self.cy
+        self.x = self.Major * math.cos(- self.periA - self.startA + self.Anglediff - self.totalw) \
+            * math.cos(self.periA) - \
+            self.Minor * math.sin(- self.periA - self.startA + self.Anglediff - self.totalw) \
+            * math.sin(self.periA) - self.cx
+        self.y = self.Major * math.cos(- self.periA - self.startA + self.Anglediff - self.totalw) \
+            * math.sin(self.periA) + \
+            self.Minor * math.sin(- self.periA - self.startA + self.Anglediff - self.totalw) \
+            * math.cos(self.periA) - self.cy
 
 
 # Open the json file with all the date of the planets
-with open('planetdata.json') as json_file:
+with open(filename) as json_file:
     data = json.load(json_file)
 
 # Call the planet class on all the planets
 planets = []
+
 for planet in data:
     Planet(data[planet]["Name"],
            float(data[planet]["Start Angle"]),
@@ -171,13 +219,13 @@ while True:
             pygame.quit()
             print(f'Refday = {RefDay}')
             for p in planets:
-                print(f'Start Angle {p.name} = {p.startA + p.totalw}')
+                print(f'Start Angle {p.name} = {(p.startA + p.totalw) % (2 * math.pi)}')
             sys.exit()
 
     # Draw the sun and every planet
     for p in planets:
         p.process()
-        pygame.draw.circle(screen, [255, 242, 222], (750, 500), radius=40)
+        pygame.draw.circle(screen, [255, 242, 222], (int(windowdim.x) / 2, int(windowdim.y) / 2), radius=p.solarradius)
 
     # Makes the movement fast until the planets are at their current positions
     if RefDay < Today:
@@ -190,33 +238,3 @@ while True:
 
     pygame.display.flip()
     fpsClock.tick(fps)
-
-
-# Mercurius = Planet(0.1698205362, 46001195.6, 1.351862225, 87.9691, 0.205630)
-# Venus = Planet(1.5123277970, 107477094, 2.296224977, 224.701, 0.006772)
-# Aarde = Planet(3.141592654, 146605913, 1.796589572, 356.256, 0.016710219)
-# Mars = Planet(2.211157629, 206744257.29, 5.874900435, 686.971, 0.0934)
-# Jupiter = Planet(0, 740599219, 0.2501231351, 4332.59, 0.0489, [184, 163, 110])
-# Saturnus = Planet(0, 1352544268.6, 1.624151042, 10755.70, 0.0565, [214, 191, 150])
-# Uranus = Planet(0, 2735561623.4, 3.019506872, 30688.5, 0.04717, [164, 191, 191])
-# Neptunus = Planet(0, 4459512525.6, 0.8399222492, 60195, 0.008678, [127, 155 212])
-# Pluto = Planet(0, 4436773649.2, 3.910846521, 90560, 0.2488, [200, 163, 132])  # niet echt
-
-# plt.plot(Mercurius.ellipsex, Mercurius.ellipsey, color='k')
-# plt.plot(Venus.ellipsex, Venus.ellipsey, color='y')
-# plt.plot(Aarde.ellipsex, Aarde.ellipsey, color='b')
-# plt.plot(Mars.ellipsex, Mars.ellipsey, color='r')
-# plt.plot(Jupiter.ellipsex, Jupiter.ellipsey, color='y')
-# plt.plot(Saturnus.ellipsex, Saturnus.ellipsey, color='y')
-# plt.plot(Uranus.ellipsex, Uranus.ellipsey, color='c')
-# plt.plot(Neptunus.ellipsex, Neptunus.ellipsey, color='b')
-# plt.plot(Pluto.ellipsex, Pluto.ellipsey, color='k')
-
-
-# sun = plt.Circle((0, 0), 10000000, color='y')
-# earth = plt.Circle((Aarde.x, Aarde.y), 10000000, color='b')
-# ax.add_patch(sun)
-# ax.add_patch(earth)
-
-
-
